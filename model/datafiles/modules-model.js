95b4c61c1d79e90443b288e6493d9e049978a4f2
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-08-18 09:00:19 +0100

    Initial commit

---
+++ model/datafiles/modules-model.js
@@ -0,0 +1,13 @@
+// Imports ---------------------------------------
+import dbConn from '../database/database.js';
+
+// Model -----------------------------------------
+const idKey = 'ModuleID';
+const mutableKeys = [
+  'ModuleName', 'ModuleCode', 'ModuleLevel', 'ModuleLeaderID', 'ModuleImageURL'
+];
+
+// Conformance -----------------------------------
+
+
+export default { dbConn, idKey, mutableKeys };
